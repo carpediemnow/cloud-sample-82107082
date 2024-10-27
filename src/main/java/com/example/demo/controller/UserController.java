33be7b9b670f13b1c927e8afc3489cc0bfa7da6d
ohib <ohib>
2024-10-27 21:30:10 +0900

    API 추가

---
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -0,0 +1,13 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+
+    @GetMapping("/api/v1/user")
+    public String getUser() {
+        return "사번";
+    }
+}
